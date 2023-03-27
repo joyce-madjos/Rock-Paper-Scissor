1fcaf0ae83ffa6d9837e4e6e955c5a742aabd719
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-03-27 19:10:48 +0800

    Added game rules

--- game.js
+++ game.js
@@ -5,9 +5,55 @@ function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3);
     return moves[randomChoice];
  }
- 
+
  function getPlayerChoice() {
     let playerChoice = prompt("You choice?");
     let lowerCaseString = playerChoice.toLowerCase();
     return lowerCaseString;
+ }
+
+ let thePlay = (playerSelection, computerSelection) => {
+	
+    let playerScore = 0;
+    let computerScore = 0;
+ 
+    if (playerSelection === "rock" && computerSelection === "paper") {
+       computerScore += 1;
+       return {
+         score_P: playerScore,
+         score_C: computerScore,
+         result: "You Lose",
+     }
+    } 
+    else if (playerSelection === "scissor" && computerSelection === "rock") {
+        computerScore += 1;
+        return {
+         score_P: playerScore,
+         score_C: computerScore,
+         result: "You Lose",
+     }
+    } 
+    else if ( playerSelection === "paper" && computerSelection === "scissor") {
+        computerScore += 1;
+        return {
+         score_P: playerScore,
+         score_C: computerScore,
+         result: "You Lose",
+     }
+    } 
+    else if (playerSelection === computerSelection) {
+     return {
+         score_P: playerScore,
+         score_C: computerScore,
+         result: "Draw",
+     }
+    } 
+    else {
+       playerScore += 1;
+       return {
+         score_P: playerScore,
+         score_C: computerScore,
+         result: "You win",
+     }
+    }
  }
