4a92aabc3f0ef934bf9853b2f6f85501c7b35bbf
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-04-03 18:37:43 +0800

    Edited UI to display computer's choice

--- game.js
+++ game.js
@@ -7,27 +7,57 @@ function getComputerChoice() {
 }
 
 
-// Image
+// Player Image
 let rockImage = document.getElementById("rockP");
 let paperImage = document.getElementById("paperP");
 let scissorImage = document.getElementById("scissorP");
+
+// Computer Image
+let computerRockImage = document.getElementById("rockC");
+let computerPaperImage = document.getElementById("paperC");
+let computerScissorImage = document.getElementById("scissorC");
 
 // Checkboxes
 let rockCheckBox = document.getElementById("rockPlayer");
 let paperCheckBox = document.getElementById("paperPlayer");
 let scissorCheckBox = document.getElementById("scissorPlayer");
 
+// Placeholder for Result
+let textResultPlayer = document.getElementById("playerChoice");
+let textResultComputer = document.getElementById("computerChoice");
+
+// Computer's choice
+let computerChoice = getComputerChoice();
+
+if(computerChoice === "rock"){
+  computerRockImage.style.backgroundColor = "blue";
+  textResultComputer.innerHTML = "ROCK";
+}
+else if(computerChoice === "paper"){
+  computerPaperImage.style.backgroundColor = "blue";
+  textResultComputer.innerHTML = "PAPER";
+}
+else if(computerChoice === "scissor"){
+  computerScissorImage.style.backgroundColor = "blue";
+  textResultComputer.innerHTML = "SCISSOR";
+}
+
 
 rockCheckBox.addEventListener("click", function() {
    theClicks(rockCheckBox, rockImage);
+   textResultPlayer.innerHTML = "ROCK";
+  //  paperCheckBox.disabled = true;
+  //  scissorCheckBox.disabled = true;
 });
 
 paperCheckBox.addEventListener("click", function() {
    theClicks(paperCheckBox, paperImage);
+   textResultPlayer.innerHTML = "PAPER";
 });
 
 scissorCheckBox.addEventListener("click",  function() {
    theClicks(scissorCheckBox, scissorImage);
+   textResultPlayer.innerHTML = "SCISSOR";
 });
 
 function theClicks(checkBoxChoice, imageChoice) {
@@ -36,6 +66,7 @@ function theClicks(checkBoxChoice, imageChoice) {
      var playerChoice = "scissor";
    } else {
       imageChoice.style.backgroundColor = "white";
+
    }
  }
 
